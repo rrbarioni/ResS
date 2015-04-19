088ccba079028feac93c9339c491ba866247c674
jpms2 <joao_pedroms@hotmail.com>
2015-04-18 23:40:35 -0300

    Update GestorCadastre.groovy

--- test/cucumber/steps/GestorCadastre.groovy
+++ test/cucumber/steps/GestorCadastre.groovy
@@ -34,7 +34,7 @@ When(~'^i fill the name field with "([^"]*)" CNPJ field with "([^"]*)", address 
 
 }
 And(~'^i click the button create account'){
-    page.selectCreatePage()
+    page.selectCreateGestor()
 }
 Then(~'^the system stores the account with CNPJ "([^"]*)"  properly $'){String cnpj ->
     gestor = GestorCadastre.findByCNPJ(cnpj)
