aa49baacf98697e298dc1e34cb7c058c84c56134
DneSteelEagle <dfm2@cin.ufpe.br>
2015-04-21 19:36:06 -0300

    MergeTest

--- grails-app/controllers/residueGenerator/ResidueGeneratorController.groovy
+++ grails-app/controllers/residueGenerator/ResidueGeneratorController.groovy
@@ -4,7 +4,6 @@ class ResidueGeneratorController {
 
     static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
 
-
     static public save(ResidueGenerator residueGenerator){
         residueGenerator.save(flush: true)
     }
--- test/functional/pages/GetPageTitle.groovy
+++ test/functional/pages/GetPageTitle.groovy
@@ -3,7 +3,6 @@ import org.springframework.web.servlet.support.RequestContextUtils as RCU
 
 class GetPageTitle {
     def messageSource
-
     String getMessage(String code) {
 
         messageSource.getMessage(code, null, "", RCU.getLocale(request));
