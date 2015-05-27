1279848b32d11d0b224051753c346a305cee3408
ArthurFreitas <abf2@cin.ufpe.br>
2015-05-26 22:52:08 -0300

    [completed#94147620]Finalização da implementação do cenário duplicated residue generator

--- test/functional/steps/GeneratorTestDataAndOperations.groovy
+++ test/functional/steps/GeneratorTestDataAndOperations.groovy
@@ -21,6 +21,7 @@ class GeneratorTestDataAndOperations{
              averageMonthlyMeals: 0,
              averageDailyMeals: 0],
 
+
     ]
 
     static public def getGenerator(){
@@ -46,6 +47,39 @@ class GeneratorTestDataAndOperations{
         def cont = new ResidueGeneratorController()
         cont.params << [name: "Clovis Palace"] << [type: "Restaurante"] << [cnpj:"testecnpj2" ] << [addressGenerator:address ] << [averageMonthlyMeals: 0] << [averageDailyMeals:0] << [id: residueGenerator.getId()]
         cont.edit()
+        cont.save()
+        cont.response.reset()
+    }
+
+    static public getAltGenerator(String address){
+        def novoGenerator =  [nameGenerator: "Alt",
+                              type: "Restaurante",
+                              cnpj: "testecnpj1",
+                              addressGenerator: address,
+                              averageMonthlyMeals: 0,
+                              averageDailyMeals: 0];
+        return novoGenerator;
+    }
+
+    static public void createAltGenerator(String address){
+        def cont = new ResidueGeneratorController()
+        def novoGenerator = getAltGenerator(address)
+        cont.params << novoGenerator
+        cont.create()
+        cont.save()
+        cont.response.reset()
+
+    }
+
+    static public void createIncompleteGenerator(String address){
+        def cont = new ResidueGeneratorController()
+        cont.params << [nameGenerator: "",
+                        type: "Restaurante",
+                        cnpj: null,
+                        addressGenerator: address,
+                        averageMonthlyMeals: null,
+                        averageDailyMeals: 0];
+        cont.create()
         cont.save()
         cont.response.reset()
     }
