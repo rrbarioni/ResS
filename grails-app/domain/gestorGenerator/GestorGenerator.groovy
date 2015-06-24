18a4b9b7a54343de427462b95928ac71d31d03f5
jpms2 <joao_pedroms@hotmail.com>
2015-06-24 19:44:05 -0300

    Mudanças básicas

--- grails-app/domain/gestorGenerator/GestorGenerator.groovy
+++ grails-app/domain/gestorGenerator/GestorGenerator.groovy
@@ -14,7 +14,7 @@ class GestorGenerator {
 
         nomeDaEmpresa (nullable: false, blank: false, unique: true );
         cNPJ (nullable: false, blank: false, unique: true)
-        telefone (nullable: false,blank: false)
+        telefone (nullable: false,blank: false,matches: "^[0-9]{8}" )
         email (nullable: false, email: true, blank: false);
         endereco (nullable: false, blank: false);
         homePage (nullable: true, blank : true);
