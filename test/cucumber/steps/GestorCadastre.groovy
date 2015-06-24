0a6d5fc229123916e609c0f10f8e6b16ebba3668
jpms2 <joao_pedroms@hotmail.com>
2015-06-24 20:19:19 -0300

    [completed#97155084]Implementar teste gestor cadastre with phone number field not containing only numbers

--- test/cucumber/steps/GestorCadastre.groovy
+++ test/cucumber/steps/GestorCadastre.groovy
@@ -6,10 +6,10 @@ import pages.GestorCreatePage
 import gestorGenerator.GestorGenerator
 import static cucumber.api.groovy.EN.*
 
+//Gestor cadastre
 Given(~'^the system has no cadastre with CNPJ "([^"]*)"$') { String cnpj ->
     gestor = GestorGenerator.findByCNPJ(cnpj)
     assert gestor == null
-
 }
 When(~'^i try to create an account with CNPJ "([^"]*)"$'){ String cNPJ ->
     cnpj = cNPJ
@@ -18,24 +18,37 @@ When(~'^i try to create an account with CNPJ "([^"]*)"$'){ String cNPJ ->
 
 Then(~'^the account is stored properly in the system$') { ->
     assert GestorTestDataAndOperations.findGestorByCnpj(cnpj) != null
-
 }
 
 
+//Gestor cadastre with Phone number field not containing only numbers
+Given(~'^i want to cadastre a new gestor with CNPJ "([^"]*)"$') {String CNPj  ->
+    assert GestorTestDataAndOperations.findGestorByCnpj(CNPj) == null
+    CNpj = CNPj
+}
+When(~'^i try to create an account with Phone number field "([^"]*)"$'){ String phone ->
+    GestorTestDataAndOperations.createGestor(CNpj)
+}
+
+Then(~'^the account with phone number "([^"]*)" is not stored in the system$') { String telefone ->
+    assert GestorTestDataAndOperations.findGestorByPhone(telefone) == null
+}
+
+//Gestor cadastre web
 Given(~'^i am at the gestor cadastre page$') { ->
     to GestorCreatePage
     at GestorCreatePage
 }
-    When(~'^i fill all the registration data fields correctly$') { ->
-        page.fillGestorDetails()
-    }
-    And(~'^i click the button "create my account"') { ->
-        page.selectCreateGestor()
-    }
-    Then(~'^the system stores the account properly$') { ->
+When(~'^i fill all the registration data fields correctly$') { ->
+    page.fillGestorDetails()
+}
+And(~'^try to create my account') { ->
+    page.selectCreateGestor()
+}
+Then(~'^i should see a message confirming the creation$') { ->
 
-    }
-    And(~'^i am redirected to the home page$') {  ->
-        to ResidueGeneratorShowPage
-        at ResidueGeneratorShowPage
-    }
+}
+And(~'^i am redirected to the home page$') {  ->
+    to ResidueGeneratorShowPage
+    at ResidueGeneratorShowPage
+}
