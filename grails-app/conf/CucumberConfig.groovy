490ff33b342744f0b6b419b65f1e1c5d47c9a574
DanielMaida <dfm2@cin.ufpe.br>
2015-09-07 14:34:31 -0300

    repo cleanup

--- grails-app/conf/CucumberConfig.groovy
+++
@@ -1,7 +0,0 @@
-cucumber {
-
-  features = ["test/cucumber"]
-  glue = features
-  tags = ["~@ignore"]
-
-}
--- test/functional/GebConfig.groovy
+++
@@ -1,9 +0,0 @@
-import org.openqa.selenium.chrome.ChromeDriver
-
-driver = {
-    File file = new File("C:/Users/danie_000/Desktop/ResS/chromedrivers/chromedriver.exe");
-    System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-    new ChromeDriver();
-}
-
-baseUrl = "http://localhost:8070/"
