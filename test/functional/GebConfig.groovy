378a5924b6786a33cb0a836ccadf6dae31da6450
João Pedro de Medeiros Santos <jpms2@cin.ufpe.br>
2015-09-25 10:12:55 -0300

    Update GebConfig.groovy

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -1,11 +1,22 @@
-import org.openqa.selenium.chrome.ChromeDriver
+/*
+	This is the Geb configuration file.
+	
+	See: http://www.gebish.org/manual/current/configuration.html
+*/
 
-driver = {
-    File file = new File("chromedrivers/chromedriver.exe");
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.firefox.FirefoxProfile
 
-
-    System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-    new ChromeDriver();
+driver = { 
+    	//set the firefox locale to 'en-us' since the tests expect english
+    	//see http://stackoverflow.com/questions/9822717 for more details
+    	FirefoxProfile profile = new FirefoxProfile()
+    	profile.setPreference("intl.accept_languages", "en-us")
+	def driverInstance = new FirefoxDriver(profile) 
+	driverInstance.manage().window().maximize() 
+	driverInstance
 }
 
+baseNavigatorWaiting = true
+atCheckWaiting = true
 baseUrl = "http://localhost:8070/"
