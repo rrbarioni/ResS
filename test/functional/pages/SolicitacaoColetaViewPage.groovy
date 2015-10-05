2764cc1d03e3e79527e1ad94175b9880f5715678
bhlvm <bhlvm@cin.ufpe.br>
2015-10-05 07:25:29 -0300

    Steps, pagina e teste

---
+++ test/functional/pages/SolicitacaoColetaViewPage.groovy
@@ -0,0 +1,23 @@
+package pages
+
+/**
+ * Created by danie_000 on 6/25/2015.
+ */
+import geb.Page
+
+class HarvestSolicitationViewPage extends Page {
+    def titulo = "Solicitacao de coleta"
+    static url = "ResS/generatorHarvestSolicitation/index/3"
+
+    static at = {
+        title ==~ titulo
+    }
+	def boolean hasOk(){
+		if($(".ok") == null){
+			return false
+		}else{
+			return true
+		}
+	}
+	
+    }
