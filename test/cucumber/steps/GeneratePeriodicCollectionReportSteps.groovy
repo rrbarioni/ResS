adcf9f64a1bc9483ca7f214547a02d8bb78f8956
Hilton Leite <hiltonpintor@gmail.com>
2015-10-21 11:13:50 -0300

    Update GeneratePeriodicCollectionReportSteps.groovy
    new methods and a few corrections

--- test/cucumber/steps/GeneratePeriodicCollectionReportSteps.groovy
+++ test/cucumber/steps/GeneratePeriodicCollectionReportSteps.groovy
@@ -1,6 +1,7 @@
 /**
  * Created by ess on 20/10/15.
  */
+ 
 package steps
 
 import pages.HarvestSolicitationViewPage
@@ -14,15 +15,12 @@ import steps.GeneratorTestDataAndOperations
 Given(~'^that collections were made on "([^"]*)" residue generator on the last month$') { String name ->
     GeneratorTestDataAndOperations.createGeneratorName(name)
     residueGenerator = ResidueGenerator.findByNameGenerator(name)
-
     assert residueGenerator != null && GeneratorTestDataAndOperations.hasMonthlyHarvest(residueGenerator)
 }
-
 When(~'^the system receives a request to generate a report by "([^"]*)" residue generator$') { String name ->
     GeneratorTestDataAndOperations.createReport(residueGenerator)
     reportGenerator = ReportGenerator.findByNameGenerator(name)
 }
-
 Then(~'^a report is generated containing those collection’s information$') { ->
     assert reportGenerator != null
 }
@@ -34,18 +32,14 @@ Given(~'^I am at the HarvestSolicitationViewPage page$'){ ->
     to HarvestSolicitationViewPage
     at HarvestSolicitationViewPage
 }
-
 When(~'I select the "Generate Report" option$') { ->
     page.generateReport()
 }
-
 And(~'collections were made on the previous month at "RU" residue generator$') { String name ->
     GeneratorTestDataAndOperations.createGeneratorName(name)
     residueGenerator = ResidueGenerator.findByNameGenerator(name)
-
     assert residueGenerator != null && GeneratorTestDataAndOperations.hasMonthlyHarvest(residueGenerator)
 }
-
 Then(~'I see the generated report$') { ->
     page.printReport()
 }
