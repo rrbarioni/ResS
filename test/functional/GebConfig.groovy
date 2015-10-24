fdc09b11c71179f7899ac602cd104ce89ee27a24
Cecĩlia Hunka <mchma@cin.ufpe.br>
2015-10-24 16:42:52 -0300

    fixing gebconfig

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -1,11 +1,26 @@
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.firefox.FirefoxProfile
 
 driver = {
-    File file = new File("chromedrivers/chromedriverlinux64");
 
-
-    System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-    new ChromeDriver();
+//def driver = new HtmlUnitDriver()
+//driver.javascriptEnabled = true
+//driver
+def driver = new FirefoxDriver()
+driver
 }
 
-baseUrl = "http://localhost:8070/"
+environments {
+   // run as “grails -Dgeb.env=chrome test-app”
+   // See: http://code.google.com/p/selenium/wiki/ChromeDriver
+   chrome {
+     driver = { new ChromeDriver() }
+   }
+
+   // run as “grails -Dgeb.env=firefox test-app”
+   // See: http://code.google.com/p/selenium/wiki/FirefoxDriver
+   firefox {
+    driver = { new FirefoxDriver() }
+  }
+}
