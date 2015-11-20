8e557685c37d89ff98c1d8055b1938ab3b508d00
Danielle Sales <dso@cin.ufpe.br>
2015-11-20 12:20:46 -0300

    corrected admin user steps

--- test/cucumber/steps/AdminUserSteps.groovy
+++ test/cucumber/steps/AdminUserSteps.groovy
@@ -34,6 +34,8 @@ Then(~'^the user "([^"]*)" is stored in the system$'){ String login ->
 GUI
 
 Given I’m at the sign up page
+And there is no user with the login "admin"
+And there is no user with the CPF "123.456.789.12"
 When I ask the system to add a user called "John Doe", with CPF "123.456.789-12", login "admin", password "abcdef", email "johndoe@johndoe.com" and phone "0000-0000"
 Then I see a confirmation message
 */
@@ -42,11 +44,27 @@ Given(~'^I’m at the sign up page$'){->
     to AdminUserSignUpPage
     at AdminUserSignUpPage
 }
+And(~'^there is no user with the login "([^"]*)"$'){ String login->
+    user = AdminUser.findByAdminLogin(login)
+    if(user != null){
+        user.delete(flush:true)
+    }
+    user = AdminUser.findByAdminLogin(login)
+    assert  user == null
+}
+And(~'^there is no user with the CPF "([^"]*)"$'){ String cpf->
+    user = AdminUser.findByAdminCpf(cpf)
+    if(user != null){
+        user.delete(flush:true)
+    }
+    user = AdminUser.findByAdminCpf(cpf)
+    assert  user == null
+}
 When(~'^I ask the system to add a user called "([^"]*)", with CPF "([^"]*)", login "([^"]*)", password "([^"]*)", email "([^"]*)" and phone "([^"]*)"$'){
     String name, String cpf, String login, String password, String email, String phone ->
         page.fillUserInfo(name, cpf, login, password, email, phone)
         page.submitUserInfo()
 }
 Then(~'^I see a message confirming the user registration$'){->
-    assert !page.hasErrors()
+    assert page.readFlashMessage() != null
 }
