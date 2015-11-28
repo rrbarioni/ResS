f1d8ab92228605d032e8b93deebf46b3e2a025d8
rrbarioni <rrb@cin.ufpe.br>
2015-11-27 21:54:04 -0300

    #107780684 #107783088 #107782894 #107783160 Changings on Coleta

--- grails-app/domain/HistoricoDeColeta/Coleta.groovy
+++ grails-app/domain/HistoricoDeColeta/Coleta.groovy
@@ -9,13 +9,15 @@ class Coleta {
     String nome // nome do estabelecimento
     Date data // data da coleta
     int volume // volume coletado
+    String senha // password do restaurante
 
 
     static constraints = {
 
         nome blank: false // ecoponto deve possuir um nome para identificação
         data blank: false, nullable: false
-        volume  blank: false, nullable: false, notEqual: 0 // volume para coleta não deve ser 0. deve-se haver algo a ser coletado
+        volume  blank: false, nullable: false, min: 1 //notEqual: 0 // volume para coleta não deve ser 0 nem negativo. deve-se haver algo a ser coletado
+        senha blank: false
     }
     void setName(String novoNome){
         if(Coleta.findByNomeAndData(novoNome,this.data) == null){
