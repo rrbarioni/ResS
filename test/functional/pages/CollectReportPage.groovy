60142b3b5593ebf38a1f0d6ee8dd877841069e23
Lucas Lucena Falbo <95.lucas@gmail.com>
2015-12-02 20:53:43 -0300

    Resolving Travis errors

---
+++ test/functional/pages/CollectReportPage.groovy
@@ -0,0 +1,24 @@
+package pages
+
+import geb.Page
+
+class CollectReportPage extends Page{
+
+    def titulo = "Relatorio de coletas"
+    static url = "ResS/coleta/genReport"
+
+    static at = {
+        title == titulo
+    }
+
+
+    def selectTodayDate(){
+        $("input", name: "date1_day").click()
+        $("input", name: "date2_day").click()
+    }
+
+    def submit(){
+        $("input", name: "Ok ").click()
+    }
+
+}
